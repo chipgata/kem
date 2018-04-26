084d9f1359fe775b288b22a5762693520383ac71
Thuong Le <thuong010790@gmail.com>
2018-04-26 13:58:34 +0700

    create and update by fixed

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,7 @@
 class Category < Base
     belongs_to :category, :foreign_key => 'parent_id', optional: true
-    belongs_to :user, :foreign_key => 'created_by', optional: true
-    belongs_to :user, :foreign_key => 'updated_by', optional: true
+    belongs_to :creater, :class_name => 'User', :foreign_key => 'created_by', optional: true
+    belongs_to :updater, :class_name => 'User', :foreign_key => 'updated_by', optional: true
 
     validates :name, presence: true,
                     length: { minimum: 2 }
--- app/models/setting.rb
+++ app/models/setting.rb
@@ -1,5 +1,5 @@
 class Setting < Base
-    belongs_to :user, :foreign_key => 'created_by', optional: true
-    belongs_to :user, :foreign_key => 'updated_by', optional: true
+    belongs_to :creater, :class_name => 'User', :foreign_key => 'created_by', optional: true
+    belongs_to :updater, :class_name => 'User', :foreign_key => 'updated_by', optional: true
 
 end
